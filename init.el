c193eec5e8f3ebd799a6aa2dae881cf6fcdea5cd
srsmith <shane.smith@altec.com>
2020-12-15 13:31:28 -0600

    Updating emacs files

--- init.el
+++ init.el
@@ -54,7 +54,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Calibri" :foundry "outline" :slant normal :weight normal :height 98 :width normal)))))
 
 ;; This command sets lines to wrap around in org-mode
 (setq org-startup-truncated nil)
