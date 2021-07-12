1f2c3830e87162ee628cefed0d42870f19472329
Shane Smith <shane.smith@altec.com>
2021-07-12 10:54:14 -0500

    Modified settings, moved themes and org into their own files.

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-clock-save.el - smith at <2021-07-09 Fri 16:44>
-(setq org-clock-stored-history '(("/home/shane/agenda/refile.org" . 20) ("/home/shane/agenda/refile.org" . 155)))
+;; org-clock-save.el - smith at <2021-07-12 Mon 10:53>
+(setq org-clock-stored-history '(("/home/shane/agenda/refile.org" . 20) ("/home/shane/agenda/refile.org" . 251)))
